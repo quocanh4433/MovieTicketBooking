12513053b77835c678e529365d9c87480ca49644
quocanh4433 <hoquocanh.td@gmail.com>
2021-09-13 10:05:48 +0700

    Initalize project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+/** Setup Redux */
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { store } from './redux/configStore'
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(rooReducer)
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
